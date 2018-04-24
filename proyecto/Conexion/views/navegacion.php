dd73c448623466d6c9fd8bed65c1da74aefbcda6
izanami saul carrasco arguelles <dragon-rojo94@hotmail.com>
2018-04-24 15:01:27 -0500

    trabajo

---
+++ proyecto/Conexion/views/navegacion.php
@@ -0,0 +1,24 @@
+<div class="col s12">  <img src="../img/header_itsx940.png" alt="" width="100%" height="195" align="baseline"/></div>
+
+    
+      
+     
+  <nav>
+    <div class="nav-wrapper blue lighten-1">
+      <a href="#"  class="brand-logo blue lighten-1">ITSX</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down blue lighten-1">
+        <li><a href="Index.php" target="_self">MENU</a><li>
+        <li><a href="Index.php?ITSX=RegistroAlumno">Registro Alumnos(a)</a></li>
+        <li><a href="Index.php?ITSX=ActividadesComplementarias">Actividades Complentarias</a></li>
+        <li><a href="Index.php?ITSX=Docentes">Registro Docentes</a></li>
+        <li><a href="Index.php?ITSX=Congresos">Congresos</a></li>
+        <li><a href="collapsible.html">Reportes</a></li>
+        <li>
+       
+       </li>
+      </ul>
+    </div>
+  </nav>
+    
+  
+ 
