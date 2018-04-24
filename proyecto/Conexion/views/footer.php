c892736e11bd56ac0b84b0e49ab24a21202f3f82
izanami saul carrasco arguelles <dragon-rojo94@hotmail.com>
2018-04-24 14:53:02 -0500

    trabajo

---
+++ proyecto/Conexion/views/footer.php
@@ -0,0 +1,26 @@
+
+<footer class="page-footer  indigo blue darken-1">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Dirección:</h5>
+                <p class="grey-text text-lighten-4">www.itsx.edu.mx</p>
+<p>Reserva Territorial SN, Col. Santa Bárbara</p> 
+<p> Xalapa, Ver. México </p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Referencias</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!"><a href="https://www.itsx.edu.mx/v2/" target="_blank">ITSX</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            Version en Español (es_mx) Copyright © 2017 Instituto Tecnológico Superior de Xalapa. All Rights Reserved    Theme by 
+
+            <a class="grey-text text-lighten-4 right" href="#!"><a href="http://www.academiathemes.com" target="_blank">AcademiaThemes</a></a>
+            </div>
+          </div>
+        </footer>
---
+++ proyecto/Conexion/views/modules/ActividadesComplementarias.php
@@ -0,0 +1,74 @@
+<form method=post action="cgi-bin/script.pl">
+<div class="container">
+	<div class="row">
+		<h1 align="center">ACTIVIDADES COMPLEMENTARIAS</h1>
+		<div class="col s12">
+			<table border="1" class="table">
+<tr>
+<td>Nombre</td>
+<td>
+<input type=text name="Nombre">
+</td>
+</tr>
+
+<tr>
+<td>FECHA DE INICIO</td>
+<td>
+<input type="date" name="fecha" min="2000-00-0"
+                                  max="2020-12-0" step="2">
+</td>
+</tr>
+
+<tr>
+<td>FECHA DE FINALIZACION</td>
+<td>
+<input type="date" name="fecha" min="2000-00-0"
+                                  max="2020-12-0" step="2">
+</td>
+</tr>
+
+<tr>
+<td>CUPO</td>
+<td>
+<input type=text name="CUPO">
+</td>
+</tr>
+
+<tr>
+<td>CREDITOS</td>
+<td>
+<input type=text name="CREDITOS">
+</td>
+</tr>
+
+<tr>
+<td>OBSERVACIONES</td>
+<td>
+<textarea rows="3" name="OBSERVACIONES">
+escriba aquí sus observaciones</textarea>
+
+</td>
+</tr>
+
+<tr>
+<td>COSTO</td>
+<td>
+<input type=text name="COSTO">
+</td>
+</tr>
+
+<tr>
+<td colspan=2>
+<a class="waves-effect waves-light btn  blue darken-1 text-darken-2">ENVIAR</a>
+
+   
+</td>
+</tr>
+</table>
+		</div>
+		
+	</div>
+</div>
+
+
+</form>
